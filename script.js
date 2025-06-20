90f30b0c747f3654bb5d9c2bc35d9793b58c271a
Harini-pusarla <147728903+Harini-pusarla@users.noreply.github.com>
2025-06-20 18:46:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function appendToDisplay(value) {
+    document.getElementById('result').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('result').value = '';
+}
+
+function deleteLast() {
+    let display = document.getElementById('result').value;
+    document.getElementById('result').value = display.slice(0, -1);
+}
+
+function calculateResult() {
+    let display = document.getElementById('result').value;
+    try {
+        document.getElementById('result').value = eval(display);
+    } catch (e) {
+        document.getElementById('result').value = 'Error';
+    }
+}
+
